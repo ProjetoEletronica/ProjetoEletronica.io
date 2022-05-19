221ec793dc50a1c0b0861cc3a5b3fbcca87a814e
ProjetoEletronica <105833461+ProjetoEletronica@users.noreply.github.com>
2022-05-18 23:08:14 -0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,31 @@
+/* Colocando senha no site
+
+var resp = window.prompt("Digite a senha")
+        while (resp!='Elisa meu amor'){
+        var resp = window.prompt('Senha incorreta, tente novamente');
+        console.log(resp);
+        }
+*/
+const lamp = window.document.getElementById('lamp');
+const turnOnOff = window.document.getElementById('turnOnOff');
+
+function LampOn(){
+    lamp.src= 'imagens/ligada.jpg';
+};
+
+function LampOff(){
+    lamp.src= 'imagens/desligada.jpg';
+};
+
+function LampOnOff(){
+    if (turnOnOff.textContent == 'Ligar'){
+        LampOn();
+        turnOnOff.textContent = 'Desligar';
+    }else{
+        LampOff()
+        turnOnOff.textContent = 'Ligar'
+    }
+}
+
+turnOnOff.addEventListener('click',LampOnOff);
+lamp.addEventListener('dblclick',Lampbreak);
